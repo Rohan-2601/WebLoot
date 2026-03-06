16831d566f7112db288976c772d53334ebeeb33c
Rohan <rjha09307@gmail.com>
2026-03-06 20:12:18 +0530

    feat: implement image extraction

---
+++ server/services/extractImages.js
@@ -0,0 +1,20 @@
+import cheerio from "cheerio"
+
+export function extractImages(html, baseUrl) {
+
+  const $ = cheerio.load(html)
+
+  let images = []
+
+  $("img").each((i, el) => {
+
+    let src = $(el).attr("src")
+
+    if (src) {
+      images.push(new URL(src, baseUrl).href)
+    }
+
+  })
+
+  return images
+}
